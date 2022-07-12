bd1ac1a697610965886da007b2f308122197e6aa
Gideon <gideonfernandez@velocitysense.com>
2022-07-12 18:55:44 +0200

    Weekly issues distro

--- email_issues_report.py
+++ email_issues_report.py
@@ -9,10 +9,11 @@ from email import encoders
 username = MMG_USER
 password = MMG_PASSWORD
 mail_from = MMG_USER
-mail_to = 'analytics@montagemarketinggroup.com, ematel@montagemarketinggroup.com, mkreider@montagemarketinggroup.com, gfernandez@montagemarketinggroup.com'
-# mail_to = 'gideonfernandez@velocitysense.com'
+# mail_to = 'analytics@montagemarketinggroup.com, ematel@montagemarketinggroup.com, mkreider@montagemarketinggroup.com, gfernandez@montagemarketinggroup.com'
+mail_to = 'conyechi@vennstrategies.com, ebradford@pyxispartners.co, elee@vennstrategies.com, hyuan@montagemarketinggroup.com, jeason@vennstrategies.com, jwallace@pyxispartners.co, jbonilla@pyxispartners.co, jccraig@montagemarketinggroup.com, jdaverne@pyxispartners.co, kcantor@pyxispartners.co, kforbes@pyxispartners.co, kconstantine@vennstrategies.com, lgilmer@montagemarketinggroup.com, rciulla@pyxispartners.co, rkmatsumoto@montagemarketinggroup.com, rtepp@pyxispartners.co, swhitaker@vennstrategies.com, wbhagat@montagemarketinggroup.com, yvelasquez@pyxispartners.co'
+mail_cc = 'analytics@montagemarketinggroup.com, ematel@montagemarketinggroup.com, mkreider@montagemarketinggroup.com, gfernandez@montagemarketinggroup.com'
 mail_subject = 'Weekly Pyxis Issues Report - ' + TODAY_SLASH 
-mail_body = 'Hi team, \n\nPlease find the weekly Pyxis Issues Report attached for activities starting June 1, 2022 and forward. \n\nRegards, \nGideon'
+mail_body = 'Hi all, \n\nPlease find the weekly Pyxis Issues Report attached for activities starting June 1, 2022 and forward. \n\nRegards, \nGideon'
 
 filepath = r'data/Pyxis Report Issues_' + TODAYSTR + '.xlsx'
 filename = r'Pyxis Report Issues_' + TODAYSTR + '.xlsx'
@@ -21,6 +22,7 @@ try:
 	mimemsg = MIMEMultipart()
 	mimemsg['From']=mail_from
 	mimemsg['To']=mail_to
+	mimemsg['Cc']=mail_cc
 	mimemsg['Subject']=mail_subject
 	mimemsg.attach(MIMEText(mail_body, 'plain'))
 
