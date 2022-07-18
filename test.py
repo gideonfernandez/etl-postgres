68433cec3d0f15acd97b00cc48e30358299b1e9d
Gideon <gideonfernandez@velocitysense.com>
2022-07-18 14:38:24 -0400

    Issure report patch for word test

--- test.py
+++
@@ -1,9 +0,0 @@
-import re
-
-def findWholeWord(w):
-    return re.compile(r'\b({0})\b'.format(w), flags=re.IGNORECASE).search
-
-print(findWholeWord('test')('em TEST delete'))    # -> <match object>
-x = findWholeWord('test')('AAFP/Research Hub Testimonial Creation (Op-ed)')
-
-print('x is:', type(x))
