ad6574963d9316f1bced3f0d9adc17eeeca1160e
Gideon <gideonfernandez@velocitysense.com>
2022-07-21 09:38:11 -0400

    testing update

--- scheduler.py
+++ scheduler.py
@@ -52,7 +52,7 @@ def issues():
     exec(open('email_issues_report.py').read())
     print('Weekly issues report completed')
 
-schedule.every().day.at('11:00').do(job)  #Time in UTC
+schedule.every().day.at('13:40').do(job)  #Time in UTC
 schedule.every().day.at('11:15').do(alerts)  #Time in UTC
 schedule.every().friday.at('21:15').do(issues)  #Time in UTC
 
